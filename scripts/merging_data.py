a68c5e092a3866448bd8a82627acfbbe887cac28
Laura <lauraflyra@gmail.com>
2021-07-19 16:19:08 +0200

    first commit, incomplete data, loading in merging file

---
+++ scripts/merging_data.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+corpus_df = pd.read_csv("../incomplete_corpus_data/PotsdamSearchCorpus.dat")
+raw_data = pd.read_csv("../incomplete_corpus_data/asc\SFC1_2.dat")
