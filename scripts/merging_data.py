f7d352102e2646c5c0858029012b92f3647573bb
Laura <lauraflyra@gmail.com>
2021-07-19 18:08:54 +0200

    jupyter notebook with function for time filtering

--- scripts/merging_data.py
+++ scripts/merging_data.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
 
-corpus_df = pd.read_csv("../incomplete_corpus_data/PotsdamSearchCorpus.dat")
-raw_data = pd.read_csv("../incomplete_corpus_data/asc\SFC1_2.dat")
+corpus_df = pd.read_csv("../incomplete_corpus_data/PotsdamSearchCorpus.dat", sep = "\t")
+#testing with raw data from participant 1
+raw_data = pd.read_csv("../incomplete_corpus_data/asc\SFC1_2.dat", sep = "\t",
+                       header = None)
