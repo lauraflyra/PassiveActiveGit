f55398a668f7fb62c6a06b87f54f448970381966
Laura <lauraflyra@gmail.com>
2021-12-08 12:57:37 +0100

    changes in merging_data and iterating files

--- scripts/merging_data.py
+++ scripts/merging_data.py
@@ -3,6 +3,7 @@ Code from Laura Lyra, 02.11.2021
 """
 
 import numpy as np
+import math
 import pandas as pd
 
 
@@ -19,11 +20,13 @@ def get_valid_times(raw_data, corpus_data):
     :return: raw_data with only valid time stamps
     """
 
-    n_trials = corpus_data['trialno'].iloc[-1]  # get number of trials
+
+    trials = np.unique(corpus_data['trialno'])
+    n_trials = len(trials)
     intervals = np.zeros((n_trials, 2))
-    for trial in range(n_trials):
-        intervals[trial, 0] = corpus_data[corpus_data['trialno'] == trial + 1]["imagestart"].iloc[0]
-        intervals[trial, 1] = corpus_data[corpus_data['trialno'] == trial + 1]["imageend"].iloc[0]
+    for idx, trial in enumerate(trials):
+        intervals[idx, 0] = corpus_data[corpus_data['trialno'] == trial]["imagestart"].iloc[0]
+        intervals[idx, 1] = corpus_data[corpus_data['trialno'] == trial]["imageend"].iloc[0]
 
     ##subtract time zero
     time_zero = corpus_data["imagestart"].iloc[0]
@@ -40,8 +43,7 @@ def get_valid_times(raw_data, corpus_data):
 
     return raw_data, np.asarray(intervals, dtype=int)
 
-
-def linear_transf_pix_dva(raw_data, pix_to_dva):
+def linear_transf_pix_dva(raw_data, pix_to_dva=0.037):
     """
     Get the raw data and transform the x-y coordinates from pixel space to dva space
     using a linear transformation.
@@ -73,27 +75,28 @@ def associate_trial_info(filtered_raw, intervals, corpus_data):
     """
     # associate image number (imageno) to the appropriate trial condition
     n_trials = intervals.shape[0]
+    trials = np.unique(corpus_data['trialno'])
     trial_durations = np.zeros(n_trials, dtype=int)
     raw_imagenos = []
     filter_type = []
     filter_region = []
     target_pres = []
     expected_loc = []
-    for trial in range(n_trials):
-        im = corpus_data[corpus_data['trialno'] == trial + 1]["imageno"].iloc[0]
-        filtertype = corpus_data[corpus_data['trialno'] == trial + 1]["filtertype"].iloc[0]
-        filterreg = corpus_data[corpus_data['trialno'] == trial + 1]["filterregion"].iloc[0]
-        targetpres = corpus_data[corpus_data['trialno'] == trial + 1]["targetpresent"].iloc[0]
-        expectedloc = corpus_data[corpus_data['trialno'] == trial + 1]["expectedlocation"].iloc[0]
+    for idx, trial in enumerate(trials):
+        im = corpus_data[corpus_data['trialno'] == trial]["imageno"].iloc[0]
+        filtertype = corpus_data[corpus_data['trialno'] == trial]["filtertype"].iloc[0]
+        filterreg = corpus_data[corpus_data['trialno'] == trial]["filterregion"].iloc[0]
+        targetpres = corpus_data[corpus_data['trialno'] == trial]["targetpresent"].iloc[0]
+        expectedloc = corpus_data[corpus_data['trialno'] == trial]["expectedlocation"].iloc[0]
 
-        trial_durations[trial] = len(filtered_raw[(filtered_raw.time >= intervals[trial][0]) &
-                                                  (filtered_raw.time <= intervals[trial][1])])
+        trial_durations[idx] = len(filtered_raw[(filtered_raw.time >= intervals[idx][0]) &
+                                                (filtered_raw.time <= intervals[idx][1])])
 
-        temp = im * np.ones([trial_durations[trial]])
-        temp_ft = filtertype * np.ones([trial_durations[trial]])
-        temp_fr = filterreg * np.ones([trial_durations[trial]])
-        temp_tp = targetpres * np.ones([trial_durations[trial]])
-        temp_el = expectedloc * np.ones([trial_durations[trial]])
+        temp = im * np.ones([trial_durations[idx]])
+        temp_ft = filtertype * np.ones([trial_durations[idx]])
+        temp_fr = filterreg * np.ones([trial_durations[idx]])
+        temp_tp = targetpres * np.ones([trial_durations[idx]])
+        temp_el = expectedloc * np.ones([trial_durations[idx]])
 
         raw_imagenos.extend(temp)
         filter_type.extend(temp_ft)
@@ -116,7 +119,6 @@ def associate_trial_info(filtered_raw, intervals, corpus_data):
 
     return filtered_raw, trial_durations
 
-
 def add_sacc_val_id(filtered_raw, corpus_data):
     """
     By comparing time stamps with the corpus_data, we get which time stamps corresponds
@@ -134,15 +136,15 @@ def add_sacc_val_id(filtered_raw, corpus_data):
 
     invalid_sacfix = ((corpus_data.fixinvalid + corpus_data.sacinvalid) >= 1).astype(int)
 
-    identifier = np.array(len(filtered_raw.time), dtype=str)
+    identifier = np.zeros(len(filtered_raw.time), dtype=str)
     is_saccade = np.ones(len(filtered_raw.time), dtype=int)
     invalid = np.zeros(len(filtered_raw.time), dtype=int)
 
     try:
         corpus_data.insert(10, "invalid_sacfix", invalid_sacfix)
         filtered_raw.insert(0, "identifier", identifier)
-        filtered_raw.insert(9, "is_saccade", is_saccade)
-        filtered_raw.insert(11, "invalid", invalid)
+        filtered_raw.insert(7, "is_saccade", is_saccade)
+        filtered_raw.insert(6, "invalid", invalid)
     except:
         pass
 
@@ -152,32 +154,54 @@ def add_sacc_val_id(filtered_raw, corpus_data):
     for image in np.unique(filtered_raw.imageno):
         imagestart = filtered_raw[filtered_raw.imageno == image].time.iloc[0]
         trialno = corpus_data[corpus_data.imageno == image].trialno.iloc[0]
-        for count, fix in enumerate(corpus_data[(corpus_data.imageno == image)].fixno):
-            invalidity = corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == fix)].invalid_sacfix
-            ident = str(subject).zfill(3) + str(trialno).zfill(3) + str(int(fix)).zfill(2)
-            fixonset = int(corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == fix)].fixonset)
-            if fixonset == 1:
-                fixonset = 0
-            fixoffset = int(corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == fix)].fixoffset)
-            filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
-                             (filtered_raw.time <= imagestart + fixoffset), "is_saccade"] = 0
+        fix_nbs = corpus_data[(corpus_data.imageno == image)].fixno
+        for count, fix in enumerate(fix_nbs):
 
-            if count < len(corpus_data[(corpus_data.imageno == image)].fixno) - 1:
-                next_fix = int(corpus_data[(corpus_data.imageno == image)].fixno.iloc[count + 1])
-                next_fixonset = int(
-                    corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == next_fix)].fixonset)
-                filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
-                                 (filtered_raw.time < imagestart + next_fixonset), "identifier"] = ident
-
-                filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
-                                 (filtered_raw.time < imagestart + next_fixonset), "invalid"] = int(invalidity.iloc[0])
+            if math.isnan(fix):
+                # this is to make sure that fixations that have NaNs have the id
+                # corresponding to the saccade in that same row and are invalid/valid
+                # for the duration of that saccade
+                c_temp = corpus_data[corpus_data.imageno == image].copy()
+                sacno = c_temp[np.logical_not(c_temp.fixno.notnull())].sacno.iloc[0]
+                saconset = c_temp[np.logical_not(c_temp.fixno.notnull())].saconset.iloc[0]
+                sacoffset = c_temp[np.logical_not(c_temp.fixno.notnull())].sacoffset.iloc[0]
+                if saconset == 1:
+                    saconset = 0
+                invalidity = 1
+                ident = "" + str(subject).zfill(3) + str(trialno).zfill(3) + str(int(sacno)).zfill(2) + ""
+                filtered_raw.loc[(filtered_raw.time >= imagestart + saconset) &
+                                 (filtered_raw.time <= imagestart + sacoffset), "identifier"] = ident
+                filtered_raw.loc[(filtered_raw.time >= imagestart + saconset) &
+                                 (filtered_raw.time <= imagestart + sacoffset), "invalid"] = int(invalidity)
 
 
             else:
+                invalidity = corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == fix)].invalid_sacfix
+                ident = "" + str(subject).zfill(3) + str(trialno).zfill(3) + str(int(fix)).zfill(2) + ""
+                fixonset = int(corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == fix)].fixonset)
+                if fixonset == 1:
+                    fixonset = 0
+                fixoffset = int(corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == fix)].fixoffset)
                 filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
-                                 (filtered_raw.time <= imagestart + fixoffset), "identifier"] = ident
-                filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
-                                 (filtered_raw.time <= imagestart + fixoffset), "invalid"] = int(invalidity.iloc[0])
+                                 (filtered_raw.time <= imagestart + fixoffset), "is_saccade"] = 0
+
+                if count < len(corpus_data[(corpus_data.imageno == image)].fixno) - 1:
+                    next_fix = int(corpus_data[(corpus_data.imageno == image)].fixno.iloc[count + 1])
+                    next_fixonset = int(
+                        corpus_data[(corpus_data.imageno == image) & (corpus_data.fixno == next_fix)].fixonset)
+                    filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
+                                     (filtered_raw.time < imagestart + next_fixonset), "identifier"] = ident
+
+                    filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
+                                     (filtered_raw.time < imagestart + next_fixonset), "invalid"] = int(
+                        invalidity.iloc[0])
+
+
+                else:
+                    filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
+                                     (filtered_raw.time <= imagestart + fixoffset), "identifier"] = ident
+                    filtered_raw.loc[(filtered_raw.time >= imagestart + fixonset) &
+                                     (filtered_raw.time <= imagestart + fixoffset), "invalid"] = int(invalidity.iloc[0])
 
         try:
             assert np.sum(corpus_data_noNaN[corpus_data_noNaN.imageno == image].sacdur) == np.sum(
@@ -189,5 +213,7 @@ def add_sacc_val_id(filtered_raw, corpus_data):
             print("image", image)
             print(np.sum(corpus_data[corpus_data.imageno == image].fixdur) + 1,
                   np.sum(filtered_raw[filtered_raw.imageno == image].is_saccade == 0))
+
+        filtered_raw['identifier'] = filtered_raw['identifier'].astype(str)
 
     return filtered_raw
